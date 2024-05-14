b0fb2892a819a9c8dd655bfa59776b4645949828
beatrizbrandt2 <129401874+beatrizbrandt2@users.noreply.github.com>
2024-05-14 08:43:58 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,41 @@
+let nome= "beatriz"
+//beatriz
+var idade= 16
+//16
+const adiçao= (nome+idade)
+//beatriz16
+  
+var ano= 2007
+let multiplicaçao= (idade*ano)
+//32.112
+ 
+let mes= 12
+//12
+var dia=19
+//19
+const subtraçao=(dia-mes)
+//7
+ 
+let nome= "beatriz"
+//beatriz
+let sobrenome= "brandt"
+//brant
+const soma=(nome+sobrenome)
+//beatriz brandt
+ 
+let nome= "beatriz"
+let data="nasceu dia"
+var dia= 19/12
+const mais=(nome+data+dia)
+//beatriz nasceu dia 19/12
+
+var meses=12
+let divisao(meses/ano)
+//165.25
+
+var comp= {dia==mes}
+//false
+
+var comp= {nome==sobrenome}
+//false
+
